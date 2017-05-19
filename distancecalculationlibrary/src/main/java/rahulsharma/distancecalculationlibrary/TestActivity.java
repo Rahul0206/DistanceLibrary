c35a36dce2c055116e0d08efe07f84120cba7c50
rahulsharma <rahul.sharma@maalgaadi.net>
2017-05-19 19:30:54 +0530

    second commit

--- distancecalculationlibrary/src/main/java/rahulsharma/distancecalculationlibrary/TestActivity.java
+++ distancecalculationlibrary/src/main/java/rahulsharma/distancecalculationlibrary/TestActivity.java
@@ -1,5 +1,8 @@
 package rahulsharma.distancecalculationlibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by Rahul on 19-May-17.
  */
@@ -7,6 +10,10 @@ package rahulsharma.distancecalculationlibrary;
 public class TestActivity {
 
     public void testMth(){
+        test1();
+    }
+
+    private void test1() {
 
     }
 
